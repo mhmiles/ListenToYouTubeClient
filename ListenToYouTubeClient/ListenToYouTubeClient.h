d7b978745707988704a7d2d801fc2e050f48803a
Miles Hollingsworth <mhmiles@me.com>
2016-07-22 12:36:56 -0700

    Initial Commit

---
+++ ListenToYouTubeClient/ListenToYouTubeClient.h
@@ -0,0 +1,19 @@
+//
+//  ListenToYouTubeClient.h
+//  ListenToYouTubeClient
+//
+//  Created by Miles Hollingsworth on 7/22/16.
+//  Copyright © 2016 Miles Hollingsworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ListenToYouTubeClient.
+FOUNDATION_EXPORT double ListenToYouTubeClientVersionNumber;
+
+//! Project version string for ListenToYouTubeClient.
+FOUNDATION_EXPORT const unsigned char ListenToYouTubeClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ListenToYouTubeClient/PublicHeader.h>
+
+
